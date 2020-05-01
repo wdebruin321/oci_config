624ad9509569ae13c3e096e6fb694088c7ae6d11
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-05-01 10:43:28 +0200

    [plan/start_instance] Initial implementation

---
+++ plans/start_instances.pp
@@ -0,0 +1,28 @@
+#
+# Use this plan to correctly shutdown an OCI instance on OS level and then stop the instance on OCI level
+#
+plan oci_config::start_instances(
+  TargetSpec $instances,
+  TargetSpec $oci_master        = 'localhost',
+  String[1]  $skip_label        = 'always_off',
+) {
+  if get_targets($oci_master).length > 1 {
+    fail_plan("${oci_master} did not resolve to a single target")
+  }
+
+  $instance_nodes = get_targets($instances)
+
+  $instance_nodes.each |$instance| {
+    $hostname = $instance.host
+    $node_facts = puppetdb_fact([$hostname])
+    if $node_facts == {} {
+      out::message("Skipping node ${instance.host} not found in Puppetdb.")
+    } elsif $node_facts.dig($hostname, 'oci_instance_id') == undef {
+      out::message("Skipping node ${instance.host} not an OCI node.")
+    } else {
+      $instance_id = $node_facts.dig($hostname, 'oci_instance_id')
+      out::message("Execute sartup on OCI-level for ${hostname} with ocid ${instance_id}...")
+      run_task('oci_config::instance_action', $oci_master, instance_id => $instance_id, action => 'START' )
+    }
+  }
+}
