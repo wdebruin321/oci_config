1e65f4344a50f76d31963e7630b296e307d2c8a3
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-05-07 15:40:30 +0200

    [oci_core_instance] Fix agent config when used with 2.8.0 gem

--- types/instanceagentconfig.pp
+++ types/instanceagentconfig.pp
@@ -1,3 +1,4 @@
 type Oci_Config::InstanceAgentConfig = Struct[{
-  Optional[is_monitoring_supported]      => Boolean,
+  Optional[is_monitoring_supported] => Boolean,
+  Optional[is_management_disabled]  => Boolean,
 }]
