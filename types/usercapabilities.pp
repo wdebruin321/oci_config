8bcc7bd3087d096d56ac1c491881e363977e0f39
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-05-07 15:38:28 +0200

    [oci_identity_user] Fix user capabilities when used with 2.8.0 gem

--- types/usercapabilities.pp
+++ types/usercapabilities.pp
@@ -1,9 +1,10 @@
 type Oci_Config::UserCapabilities = Struct[{
-  Optional[can_use_console_password] => Boolean,
-  Optional[can_use_api_keys] => Boolean,
-  Optional[can_use_auth_tokens] => Boolean,
-  Optional[can_use_smtp_credentials] => Boolean,
-  Optional[can_use_customer_secret_keys] => Boolean,
+  Optional[can_use_console_password]           => Boolean,
+  Optional[can_use_api_keys]                   => Boolean,
+  Optional[can_use_auth_tokens]                => Boolean,
+  Optional[can_use_smtp_credentials]           => Boolean,
+  Optional[can_use_customer_secret_keys]       => Boolean,
+  Optional[can_use_o_auth2_client_credentials] => Boolean,
 }]
 
 
