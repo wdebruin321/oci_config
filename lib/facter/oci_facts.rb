1619b3c4b976cd69ed62965a1643f8e4e94f7a3f
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-05-14 09:24:25 +0200

    [facts] Fix requires

--- lib/facter/oci_facts.rb
+++ lib/facter/oci_facts.rb
@@ -4,10 +4,10 @@
 # See the file "LICENSE" for the full license governing this code.
 #
 begin
-  require "#{File.dirname(__FILE__)}/../puppet_x/enterprisemodules/monkey_patches/dir"
+  require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/dir'
   require 'oci'
-  require "#{File.dirname(__FILE__)}/../puppet_x/enterprisemodules/oci/monkey_patches/oci"
-  require "#{File.dirname(__FILE__)}/../puppet_x/enterprisemodules/oci/define_fact"
+  require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/oci'
+  require_relative '../puppet_x/enterprisemodules/oci/define_fact'
 
   oci_define_fact(:oci_core_public_ip, %w[ip_address time_created])
   oci_define_fact(:oci_database_autonomous_database, %w[time_created db_name service_console_url connection_strings connection_urls db_version])
