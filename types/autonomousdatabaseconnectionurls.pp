905b95bd086a009317a76a353fc7faef082e88cf
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-06-12 12:21:13 +0200

    [oci_database_autonomous_database] fix error unrecognized key 'machine_learning_user_management_url'

--- types/autonomousdatabaseconnectionurls.pp
+++ types/autonomousdatabaseconnectionurls.pp
@@ -1,5 +1,6 @@
 # The details of an autonomous Database connection url
 type Oci_Config::AutonomousDatabaseConnectionUrls = Struct[{
-  Optional['sql_dev_web_url'] => String[1],
-  Optional['apex_url']        => String[1],
+  Optional['sql_dev_web_url']                      => String[1],
+  Optional['apex_url']                             => String[1],
+  Optional['machine_learning_user_management_url'] => String[1],
 }]
