b8e20293848380fbbbd82f77ea26de276b7ca31d
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-11-19 13:37:28 +0100

    [tasks/get_metadata] Renamed from get resource_data

---
+++ tasks/get_metadata.rb
@@ -0,0 +1,40 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+# frozen_string_literal: true
+
+#
+#
+# See the file "LICENSE" for the full license governing this code.
+#
+#
+$LOAD_PATH << "#{__dir__}/../lib"
+$LOAD_PATH << "#{__dir__}/../../easy_type/lib"
+require 'puppet_x/enterprisemodules/oci/oci_task'
+require 'puppet_x/enterprisemodules/oci/service_info'
+
+ServiceInfo = Puppet_X::EnterpriseModules::Oci::ServiceInfo
+
+# TODO: Docs
+# docs
+class GetMetadataTask < Puppet_X::EnterpriseModules::Oci::PuppetTask
+  parameter :resource_name
+  parameter :resource_id
+  parameter :ocid_type
+  parameter :field_selection
+
+  oci_info  :resource_name
+
+  def call_oci
+    fail 'We need either a resource name or a resource id' if @resource_id.nil?
+
+    ocid = if @resource_name
+             @resolver.name_to_ocid(@tenant, @resource_name, @ocid_type.to_sym)
+           else
+             @resource_id
+           end
+    client = client_for(ServiceInfo.id_to_client(@ocid_type.to_sym), @tenant)
+    data = client.send("get_#{@ocid_type}", ocid).data.to_hash.to_puppet
+    @field_selection.nil? ? data : data[*@field_selection.split('.')]
+  end
+end
+
+GetMetadataTask.run if __FILE__ == $PROGRAM_NAME
