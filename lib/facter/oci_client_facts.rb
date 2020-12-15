efee9c7d55ba2e1ed75f4abefc832a373d8bc112
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-12-15 15:54:38 +0100

    [facts] Fix fact resolution

--- lib/facter/oci_client_facts.rb
+++ lib/facter/oci_client_facts.rb
@@ -6,6 +6,7 @@
 require 'net/http'
 require 'json'
 require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/hash'
+require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/string'
 
 def instance_path
   '/opc/v1/instance/'
