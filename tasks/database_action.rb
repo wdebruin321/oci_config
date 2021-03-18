4664b71fc0b8f061b2c2f29c32eff19742793567
Bert Hajee <bert.hajee@enterprisemodules.com>
2021-03-18 16:07:14 +0100

    [tasks/database_action] Add retry parameter

--- tasks/database_action.rb
+++ tasks/database_action.rb
@@ -17,15 +17,23 @@ class DataabaseActionTask < Puppet_X::EnterpriseModules::Oci::PuppetTask
   parameter :db_node_id
   parameter :action, 'START'
   oci_info  :db_node_name
+  parameter :max_retries, 1
   client    OCI::Database::DatabaseClient
 
   def call_oci
+    retries = 0
     ocid = if @db_node_name
              @resolver.name_to_ocid(@tenant, @db_node_name, :dbnode)
            else
              @db_node_id
            end
-    @client.db_node_action(ocid, @action).data
+    begin
+      @client.db_node_action(ocid, @action).data
+    rescue OCI::Errors::ServiceError
+      retries += 1
+      retry if retries <= @max_retries
+      raise
+    end
   end
 end
 
