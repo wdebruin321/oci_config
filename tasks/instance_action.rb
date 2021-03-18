907ac2031729a6167dced5d7e9c75f990f5287bf
Bert Hajee <bert.hajee@enterprisemodules.com>
2021-03-18 16:06:37 +0100

    [tasks/instance_action] Add retry parameter

--- tasks/instance_action.rb
+++ tasks/instance_action.rb
@@ -16,16 +16,24 @@ class InstanceActionTask < Puppet_X::EnterpriseModules::Oci::PuppetTask
   parameter :instance_name
   parameter :instance_id
   parameter :action, 'START'
+  parameter :max_retries, 1
   oci_info  :instance_name
   client    OCI::Core::ComputeClient
 
   def call_oci
+    retries = 0
     ocid = if @instance_name
              @resolver.name_to_ocid(@tenant, @instance_name, :instance)
            else
              @instance_id
            end
-    @client.instance_action(ocid, @action).data
+    begin
+      @client.instance_action(ocid, @action).data
+    rescue OCI::Errors::ServiceError
+      retries += 1
+      retry if retries <= @max_retries
+      raise
+    end
   end
 end
 
