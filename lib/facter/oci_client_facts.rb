5692160f73ce98157367f570b5fb9cca078edf66
Wouterdebruin <w.de.bruin@truston.com>
2025-06-12 11:24:00 +0200

    test

--- lib/facter/oci_client_facts.rb
+++ lib/facter/oci_client_facts.rb
@@ -3,10 +3,145 @@
 #
 # See the file "LICENSE" for the full license governing this code.
 #
+# require 'net/http'
+# require 'json'
+# require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/hash'
+# require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/string'
+
+# def instance_path
+#   '/opc/v2/instance/'
+# end
+
+# def vnic_path
+#   '/opc/v2/vnics/'
+# end
+
+# def add_authorization_header(request)
+#   request['Authorization'] = "Bearer Oracle"
+# end
+
+# def get_data(path)
+#   request = Net::HTTP::Get.new(URI("http://169.254.169.254#{path}"))
+#   add_authorization_header(request)
+#   response = Net::HTTP.start('169.254.169.254', 80, :read_timeout => 0.5, :open_timeout => 0.5) do |http|
+#     http.read_timeout = 0.5
+#     http.get(path)
+#     http.request(request)
+#   end
+#   json_data = response.body
+#   begin
+#     data = JSON.parse(json_data)
+#     convert_keys(data)
+#   rescue JSON::ParserError
+#     nil
+#   end
+# end
+
+# def convert_keys(value)
+#   case value
+#   when Array
+#     value.map { |v| convert_keys(v) }
+#   when Hash
+#     value.to_puppet
+#   else
+#     value
+#   end
+# end
+
+# def oci_try
+#   value = yield
+#   Puppet.debug 'oci metadata fetched successfully.'
+#   value
+# rescue StandardError
+#   Puppet.debug 'oci meta data URL not responding. Infering not on OCI.'
+#   nil
+# end
+
+# def vnic_data
+#   oci_try { get_data(vnic_path) }
+# end
+
+# def instance_data
+#   oci_try { get_data(instance_path) }
+# end
+
+# Facter.add(:oci_instance) do
+#   setcode do
+#     instance_data
+#   end
+# end
+
+# Facter.add(:oci_vnics) do
+#   setcode do
+#     vnic_data
+#   end
+# end
+
+# Facter.add(:oci_instance_id) do
+#   confine :oci_instance do |oci_instance|
+#     !oci_instance.nil?
+#   end
+
+#   oci_instance = Facter.value(:oci_instance)
+#   setcode do
+#     oci_instance['id']
+#   end
+# end
+
+# Facter.add(:oci_defined_tags) do
+#   confine :oci_instance do |oci_instance|
+#     !oci_instance.nil?
+#   end
+
+#   oci_instance = Facter.value(:oci_instance)
+#   setcode do
+#     oci_instance['defined_tags']
+#   end
+# end
+
+# Facter.add(:oci_freeform_tags) do
+#   confine :oci_instance do |oci_instance|
+#     !oci_instance.nil?
+#   end
+
+#   oci_instance = Facter.value(:oci_instance)
+#   setcode do
+#     oci_instance['freeform_tags']
+#   end
+# end
+
+# Facter.add(:oci_ocpus) do
+#   confine :oci_instance do |oci_instance|
+#     !oci_instance.nil?
+#   end
+
+#   setcode do
+#     begin
+#       require 'oci'
+#       Facter.debug("OCI gem geladen")
+
+#       signer = OCI::Auth::Signers::InstancePrincipalsSecurityTokenSigner.new
+#       compute_client = OCI::Core::ComputeClient.new(signer: signer)
+
+#       instance_id = Facter.value(:oci_instance)['id']
+#       Facter.debug("Instance ID: #{instance_id}")
+#       instance = compute_client.get_instance(instance_id).data
+#       ocpus = instance.shape_config.ocpus
+#       Facter.debug("OCPUs: #{ocpus}")
+#       ocpus
+#     rescue LoadError => le
+#       Facter.debug("Could not load OCI gem: #{le}")
+#       nil
+#     rescue => e
+#       Facter.debug("Failed to get ocpus: #{e}")
+#       nil
+#     end
+#   end
+# end
+# frozen_string_literal: true
+
 require 'net/http'
 require 'json'
-require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/hash'
-require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/string'
 
 def instance_path
   '/opc/v2/instance/'
@@ -17,123 +152,69 @@ def vnic_path
 end
 
 def add_authorization_header(request)
-  request['Authorization'] = "Bearer Oracle"
+  request['Authorization'] = 'Bearer Oracle'
 end
 
 def get_data(path)
   request = Net::HTTP::Get.new(URI("http://169.254.169.254#{path}"))
   add_authorization_header(request)
-  response = Net::HTTP.start('169.254.169.254', 80, :read_timeout => 0.5, :open_timeout => 0.5) do |http|
+  response = Net::HTTP.start('169.254.169.254', 80, read_timeout: 0.5, open_timeout: 0.5) do |http|
     http.read_timeout = 0.5
-    http.get(path)
     http.request(request)
   end
   json_data = response.body
   begin
-    data = JSON.parse(json_data)
-    convert_keys(data)
+    JSON.parse(json_data)
   rescue JSON::ParserError
+    puts "Could not parse JSON from #{path}" if ENV['FACTER_DEBUG']
     nil
   end
 end
 
-def convert_keys(value)
-  case value
-  when Array
-    value.map { |v| convert_keys(v) }
-  when Hash
-    value.to_puppet
-  else
-    value
-  end
-end
-
-def oci_try
-  value = yield
-  Puppet.debug 'oci metadata fetched successfully.'
-  value
-rescue StandardError
-  Puppet.debug 'oci meta data URL not responding. Infering not on OCI.'
+def oci_try(&block)
+  block.call
+rescue StandardError => e
+  puts "OCI metadata not available: #{e.message}" if ENV['FACTER_DEBUG']
   nil
 end
 
-def vnic_data
-  oci_try { get_data(vnic_path) }
-end
-
-def instance_data
-  oci_try { get_data(instance_path) }
-end
-
 Facter.add(:oci_instance) do
-  setcode do
-    instance_data
-  end
+  setcode { oci_try { get_data(instance_path) } }
 end
 
 Facter.add(:oci_vnics) do
-  setcode do
-    vnic_data
-  end
+  setcode { oci_try { get_data(vnic_path) } }
 end
 
 Facter.add(:oci_instance_id) do
-  confine :oci_instance do |oci_instance|
-    !oci_instance.nil?
-  end
-
-  oci_instance = Facter.value(:oci_instance)
-  setcode do
-    oci_instance['id']
-  end
+  confine :oci_instance do |oci_instance| !oci_instance.nil? end
+  setcode { Facter.value(:oci_instance)['id'] }
 end
 
 Facter.add(:oci_defined_tags) do
-  confine :oci_instance do |oci_instance|
-    !oci_instance.nil?
-  end
-
-  oci_instance = Facter.value(:oci_instance)
-  setcode do
-    oci_instance['defined_tags']
-  end
+  confine :oci_instance do |oci_instance| !oci_instance.nil? end
+  setcode { Facter.value(:oci_instance)['defined_tags'] }
 end
 
 Facter.add(:oci_freeform_tags) do
-  confine :oci_instance do |oci_instance|
-    !oci_instance.nil?
-  end
-
-  oci_instance = Facter.value(:oci_instance)
-  setcode do
-    oci_instance['freeform_tags']
-  end
+  confine :oci_instance do |oci_instance| !oci_instance.nil? end
+  setcode { Facter.value(:oci_instance)['freeform_tags'] }
 end
 
 Facter.add(:oci_ocpus) do
-  confine :oci_instance do |oci_instance|
-    !oci_instance.nil?
-  end
-
   setcode do
     begin
       require 'oci'
-      Facter.debug("OCI gem geladen")
-
       signer = OCI::Auth::Signers::InstancePrincipalsSecurityTokenSigner.new
       compute_client = OCI::Core::ComputeClient.new(signer: signer)
-
       instance_id = Facter.value(:oci_instance)['id']
-      Facter.debug("Instance ID: #{instance_id}")
       instance = compute_client.get_instance(instance_id).data
-      ocpus = instance.shape_config.ocpus
-      Facter.debug("OCPUs: #{ocpus}")
-      ocpus
+      instance.shape_config.ocpus
     rescue LoadError => le
-      Facter.debug("Could not load OCI gem: #{le}")
+      puts "Could not load OCI gem: #{le}" if ENV['FACTER_DEBUG']
       nil
     rescue => e
-      Facter.debug("Failed to get ocpus: #{e}")
+      puts "Failed to get OCPUs: #{e}" if ENV['FACTER_DEBUG']
       nil
     end
   end
